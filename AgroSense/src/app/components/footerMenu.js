6848f9bac77384dd90bbee3fbd5ba4b3fb698707
KennRod <rodriguezjosh2003@gmail.com>
2024-09-22 21:58:35 -0600

    New icons footer

--- AgroSense/src/app/components/footerMenu.js
+++ AgroSense/src/app/components/footerMenu.js
@@ -9,15 +9,15 @@ const FooterMenu = ({ navigation }) => {
         <Text style={styles.menuText}>Inicio</Text>
       </TouchableOpacity>
       <TouchableOpacity onPress={() => navigation.navigate('Cultivos')} style={styles.menuItem}>
-        <Image source={require('../../app/assets/cultivos-48.png')} style={styles.icon} />
+        <Image source={require('../../app/assets/crops.png')} style={styles.icon} />
         <Text style={styles.menuText}>Sus cultivos</Text>
       </TouchableOpacity>
       <TouchableOpacity onPress={() => navigation.navigate('ChatBot')} style={styles.menuItem}>
-        <Image source={require('../../app/assets/chatbot-48.png')} style={styles.icon} />
+        <Image source={require('../../app/assets/chatbot.png')} style={styles.icon} />
         <Text style={styles.menuText}>ChatBot</Text>
       </TouchableOpacity>
       <TouchableOpacity onPress={() => navigation.navigate('Biblioteca')} style={styles.menuItem}>
-        <Image source={require('../../app/assets/biblioteca-48.png')} style={styles.icon} />
+        <Image source={require('../../app/assets/library.png')} style={styles.icon} />
         <Text style={styles.menuText}>Biblioteca agrícola</Text>
       </TouchableOpacity>
     </View>
