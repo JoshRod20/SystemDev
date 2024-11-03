6a59cc4ad25aa3f452e97e53225e576afecaa4b7
KennRod <rodriguezjosh2003@gmail.com>
2024-11-03 12:37:24 -0600

    redirect links in library

--- AgroSense/src/app/components/library.js
+++ AgroSense/src/app/components/library.js
@@ -1,7 +1,7 @@
-import { Text, View, Image, StyleSheet } from "react-native";
 import React from "react";
+import { Text, View, Image, StyleSheet, TouchableOpacity } from "react-native";
 
-const Library = ({ picture, info }) => {
+const Library = ({ picture, info, onPressButton }) => {
   return (
     <View style={styles.libraryContainer}>
       <View style={styles.imageContainer}>
@@ -9,6 +9,9 @@ const Library = ({ picture, info }) => {
       </View>
       <View style={styles.textContainer}>
         <Text style={styles.info}>{info}</Text>
+        <TouchableOpacity style={styles.libraryButton} onPress={onPressButton}>
+          <Text style={styles.buttonText}>Más información</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -32,7 +35,6 @@ const styles = StyleSheet.create({
     },
     shadowOpacity: 0.39,
     shadowRadius: 8.3,
-
     elevation: 13,
   },
   imageContainer: {
@@ -47,11 +49,37 @@ const styles = StyleSheet.create({
     fontSize: 16,
     textAlign: "left",
     marginVertical: 5,
-    paddingBottom: 40,
+    paddingBottom: 20,
   },
   textContainer: {
     flex: 2,
     alignSelf: "center",
+  },
+  libraryButton: {
+    backgroundColor: "#B7D2BF", // Color de fondo del botón
+    borderRadius: 17,
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    alignItems: "center",
+    marginTop: 5,
+    height: 35,
+    width: 170,
+    marginLeft: 30,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.39,
+    shadowRadius: 8.3,
+    elevation: 13,
+  },
+  buttonText: {
+    color: "black", // Color del texto del botón
+    textAlign: 'center',
+    fontSize: 15,
+    fontWeight: "bold",
+    paddingVertical: 2,
   },
 });
 
--- AgroSense/src/app/screens/agriculturalLibrary.js
+++ AgroSense/src/app/screens/agriculturalLibrary.js
@@ -1,13 +1,14 @@
 import React, { useState, useRef } from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  ScrollView,
-  TextInput,
-} from "react-native";
+import { View, Text, StyleSheet, ScrollView, Linking } from "react-native";
 import FooterMenu from "../components/footerMenu";
 import Library from "../components/library";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { firebaseApp } from "../services/firebase"; 
+import { LogBox } from 'react-native';
+
+const db = getFirestore(firebaseApp);
+
+LogBox.ignoreAllLogs(true); // Ignorar todas las advertencias
 
 const AgriculturalLibrary = ({ navigation }) => {
   const [showFooter, setShowFooter] = useState(true);
@@ -30,6 +31,21 @@ const AgriculturalLibrary = ({ navigation }) => {
     previousScrollOffset.current = currentScrollOffset;
   };
 
+  // Función para manejar el clic en el botón y redirigir al enlace de Firestore
+  const handleButtonPress = async (fieldName) => {
+    try {
+      const querySnapshot = await getDocs(collection(db, "biblioteca_agricola"));
+      querySnapshot.forEach((doc) => {
+        const link = doc.data()[fieldName]; // Obtiene el enlace del campo correspondiente
+        if (link) {
+          Linking.openURL(link);
+        }
+      });
+    } catch (error) {
+      console.error("Error al obtener los documentos de la colección:", error);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.textContainer}>
@@ -45,22 +61,27 @@ const AgriculturalLibrary = ({ navigation }) => {
         <Library
           picture={require("../assets/caf--icono-de-cultivo--removebg-preview.png")}
           info="El café es una de las bebidas más populares. Sin embargo, detrás de cada taza de café se encuentra un proceso de cultivo que requiere cuidado y atención constantes... "
+          onPressButton={() => handleButtonPress("Cafe")} // Enlace al campo 'Cafe'
         />
         <Library
           picture={require("../assets/frijoles-icono-de-cultivo__1_-removebg-preview.png")}
-          info="La planta de frijol es muy suceptible a condiciones extremas; exceso o falta de humedad, por tal razón debe... "
+          info="La planta de frijol es muy susceptible a condiciones extremas; exceso o falta de humedad, por tal razón debe... "
+          onPressButton={() => handleButtonPress("Frijol")} // Enlace al campo 'Frijol'
         />
         <Library
           picture={require("../assets/icono-de-citricos--removebg-preview.png")}
           info="Dependiendo del tipo de árbol que hayamos establecido, vamos a tener el tiempo de cosecha, siendo de 5 a 7 años en la mayoría de especies cítricas... "
+          onPressButton={() => handleButtonPress("Citricos")} // Enlace al campo 'Citricos'
         />
         <Library
           picture={require("../assets/icon Maiz.png")}
           info="El maíz es el cereal más producido en el mundo, por lo que se destaca de otros cultivos. En los últimos 50 años, la producción de este grano ha ido en aumento... "
+          onPressButton={() => handleButtonPress("Maiz")} // Enlace al campo 'Maiz'
         />
         <Library
           picture={require("../assets/icon Arroz.png")}
           info="Durante las últimas décadas, la producción de arroz ha crecido significativamente gracias a avances en la tecnología agrícola y métodos de cultivo... "
+          onPressButton={() => handleButtonPress("Arroz")} // Enlace al campo 'Arroz'
         />
       </ScrollView>
       {showFooter && <FooterMenu navigation={navigation} />}
